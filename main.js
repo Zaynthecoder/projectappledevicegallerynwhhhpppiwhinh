6515bd48c95cbbf8f958479040f1ef41c338bcd1
Zaynthecoder <68971876+Zaynthecoder@users.noreply.github.com>
2021-08-19 14:23:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setDate(){
+    document.getElementById('date').innerHTML = new Date().toDateString();
+}
+
+function twelveinfo() {
+    window.alert("This is the iPhone 12 Pro. There are four models. iPhone 12 mini, iPhone 12, iPhone 12 Pro, and iPhone 12 Pro Max. iPhone 12s start at $700");
+}
+
+function eleveninfo() {
+    window.alert("This is the iPhone 11 Pro Max. The iPhone 11 have 3 models, the iPhone 11, iPhone 11 Pro, and iPhone 11 Pro Max. they are an older model. starting at $600");
+
+}
+function mbookairinfo() {
+    window.alert("This is the M1 Macbook Air. It starts at $1000. It has the superfast Custom Designed Apple M1. Its nice");
+
+}
+function ipadproinfo() {
+    window.alert("This is the iPad Pro. It starts at $800. It has the M1 Processor.")
+}
